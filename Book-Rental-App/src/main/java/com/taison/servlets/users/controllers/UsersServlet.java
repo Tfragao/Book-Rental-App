81771788750c9a58b710dd3a9d5ba8a75b8ae3b9
Tfragao <fragaot@gmail.com>
2022-06-16 21:23:16 +0200

    users control logic added.

---
+++ Book-Rental-App/src/main/java/com/taison/servlets/users/controllers/UsersServlet.java
@@ -0,0 +1,29 @@
+package com.taison.servlets.users.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.taison.servlets.users.dao.DaoUserMemory;
+
+@WebServlet("/users")
+public class UsersServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("users", DaoUserMemory.getUserInstance().getAllUsers());
+		
+		request.getRequestDispatcher("/WEB-INF/views/users.jsp").forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
