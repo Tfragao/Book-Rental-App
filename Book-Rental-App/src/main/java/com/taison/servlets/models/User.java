a61ab1f7ff90acc614b7dab33689dc3153138263
Tfragao <fragaot@gmail.com>
2022-06-16 20:13:42 +0200

    user model implemented.

---
+++ Book-Rental-App/src/main/java/com/taison/servlets/models/User.java
@@ -0,0 +1,64 @@
+package com.taison.servlets.models;
+
+import java.util.Objects;
+
+public class User {
+	private Long id;
+	private String email;
+	private String password;
+	
+	public User(Long id, String email, String password) {
+		setId(id);
+		setEmail(email);
+		setPassword(password);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, password);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id)
+				&& Objects.equals(password, other.password);
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", email=" + email + ", password=" + password + "]";
+	}
+	
+	
+}
