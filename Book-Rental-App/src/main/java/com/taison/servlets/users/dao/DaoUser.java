22ce2aab4f333ee9163e82b28b17a54d2067fa29
Tfragao <fragaot@gmail.com>
2022-06-22 13:09:52 +0200

    get by email added.

--- Book-Rental-App/src/main/java/com/taison/servlets/users/dao/DaoUser.java
+++ Book-Rental-App/src/main/java/com/taison/servlets/users/dao/DaoUser.java
@@ -1,7 +1,10 @@
 package com.taison.servlets.users.dao;
 
+import com.taison.servlets.dao.DaoException;
 import com.taison.servlets.models.User;
 
 public interface DaoUser extends Dao<User> {
-
+	default User getByEmail(String email) {
+		throw new DaoException("NOT IMPLEMENTED");
+	}
 }
