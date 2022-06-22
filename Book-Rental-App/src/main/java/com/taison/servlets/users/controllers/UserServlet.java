8c70ef9688e7f0313209e440455179095e5f5d7f
Tfragao <fragaot@gmail.com>
2022-06-22 13:13:40 +0200

    users and user controlled modified.

--- Book-Rental-App/src/main/java/com/taison/servlets/users/controllers/UserServlet.java
+++ Book-Rental-App/src/main/java/com/taison/servlets/users/controllers/UserServlet.java
@@ -7,19 +7,57 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import com.taison.servlets.models.User;
 
+@WebServlet("/admin/user")
 public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String id = request.getParameter("id");
+		
+		if(id != null) {
+			User user = Global.DAO.getUsersById(Long.parseLong(id));
+			request.setAttribute("user", user);
+		}
+		
+		request.getRequestDispatcher("/WEB-INF/views/user.jsp").forward(request, response);
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		
+		String id = request.getParameter("id");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		User user = new User(null, email, password);
+		
+		String op = "";
+		
+		try {
+			
+				if(id == null || id.trim().length() == 0) {
+					op = "added";
+					Global.DAO.insertUser(user);
+				}else {
+					op = "modified";
+					user.setId(Long.parseLong(id));
+					Global.DAO.modifyUser(user);
+				}
+				
+				request.setAttribute("alertText", "It has been " + op + "the register correctly");
+				request.setAttribute("levelAlert", "success");
+			
+		}catch(NumberFormatException e) {
+			request.setAttribute("alertText", "The option of " + op + "did not work");
+			request.setAttribute("levelAlert", "danger");
+		}
+		
+		request.getRequestDispatcher("/admin/users").forward(request, response);
 	}
 
 }
--- Book-Rental-App/src/main/java/com/taison/servlets/users/controllers/UsersServlet.java
+++ Book-Rental-App/src/main/java/com/taison/servlets/users/controllers/UsersServlet.java
@@ -7,15 +7,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import com.taison.servlets.users.dao.DaoUserMemory;
 
-@WebServlet("/users")
+@WebServlet("/admin/users")
 public class UsersServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("users", DaoUserMemory.getUserInstance().getAllUsers());
+		request.setAttribute("users", Global.DAO.getAllUsers());
 		
 		request.getRequestDispatcher("/WEB-INF/views/users.jsp").forward(request, response);
 	}
