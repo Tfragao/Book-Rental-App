27d3bd9561d94ff553a1c61c30c4b00ac19a0ad5
Tfragao <fragaot@gmail.com>
2022-06-22 13:10:26 +0200

    Servlet to delete users added.

---
+++ Book-Rental-App/src/main/java/com/taison/servlets/users/controllers/DeleteUserServlet.java
@@ -0,0 +1,33 @@
+package com.taison.servlets.users.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/admin/delete")
+public class DeleteUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String id = request.getParameter("id");
+		
+		Global.DAO.deleteUser(Long.parseLong(id));
+		
+		request.setAttribute("alertText", "The register has been deleted " + id + "correctly");
+		request.setAttribute("levelAlert", "success");
+		
+		request.getRequestDispatcher("/admin/users").forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
